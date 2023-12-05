9bd781d876ebc045b40205b4e49fd49523de03d1
Florence Affoh <123florenceaffoh@gmail.com>
2023-12-05 15:07:15 +0000

    Modified the api file

--- src/demo_01/app.py
+++ src/demo_01/app.py
@@ -3,6 +3,7 @@ import joblib
 from pydantic import BaseModel
 import pandas as pd
 import uvicorn
+import numpy as np
 
 # Load the model
 model = joblib.load("model/rf_model.joblib")
@@ -21,19 +22,24 @@ class SepsisInput(BaseModel):
     Blood_work_results4: float
     Age: int
 
-# Define the root endpoint
+# Status endpoint : check if the api is online
+@app.get("/status")
+async def status():
+    return {"message": "online"}
+
+# the root endpoint
 @app.get("/") 
 async def root():
     return {"info": "Sepsis Prediction API: This API application serves as a bridge between the model and the user" }
 
-# Define the home endpoint
+# the home endpoint
 @app.get('/home')
 def home():
     return {'message': 'Welcome to the Sepsis Prediction Model'}
 
-# Define the prediction endpoint
+# the prediction endpoint
 @app.post("/predict")
-async def predict_sepsis(sepsis: SepsisInput):
+async def predict_sepsis(sepsis:SepsisInput,Plasma_glucose: int, Blood_work_results1: int, Blood_Pressure: int, Blood_work_results2: int, Blood_work_results3: int, Body_mass_Index: float, Blood_work_results4: float, Age: int):
     try:
         # Map the input feature names to the model's expected feature names
         input_data = {
@@ -55,10 +61,30 @@ async def predict_sepsis(sepsis: SepsisInput):
         sepsis_status = "Patient is susceptible to sepsis" if prediction == 1 else "Patient is not susceptible to sepsis"
 
         # Calculate confidence score
-        confidence_score = model.predict_proba(input_df).max()
+        confidence_score = model.predict_proba(input_df)[0]
+        probability = confidence_score[1]  # Probability of positive sepsis prediction
 
-        # Return the prediction and confidence score
-        return {"prediction": sepsis_status, "confidence_score": confidence_score, "execution_msg": "The execution went fine", "execution_code": 1}
+       # Define status icon and explanation based on the prediction
+        if prediction == 1:
+            status_icon = "✘"  # Red 'X' icon for positive sepsis prediction
+            sepsis_explanation = "A high prediction for sepsis indicates that the patient may be experiencing severe symptoms due to an infection and requires immediate medical attention."
+        else:
+            status_icon = "✔"  # Green checkmark icon for negative sepsis prediction
+            sepsis_explanation = "A low prediction for sepsis suggests that the patient is not currently showing symptoms of the condition."
+
+        # Construct the statement
+        statement = f"{status_icon} {sepsis_status} with a probability of {probability:.2f}. {sepsis_explanation}"
+
+        # Construct the result
+        result = {
+            'predicted_sepsis': sepsis_status,
+            'statement': statement,
+            'execution_msg': "The execution went fine",
+            'execution_code': 1
+        }
+
+        # Return the result
+        return result
 
     except Exception as e:
         print(f"Something went wrong during the sepsis prediction: {e}")
